2eff2b9edf91733954de75989e9f5fd12abb0ccc
lelouch <840025951@163.com>
2016-10-13 23:16:14 +0800

    Create Hash MD5 project

---
+++ src/HashMd5.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : HashMd5.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hash Md5" << endl; // prints Hash Md5
+	return 0;
+}
